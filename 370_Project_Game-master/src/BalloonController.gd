adcc5c496f519d90f587a5ffc03546bb7ab2977c
Julian Gomez <jgomez13@email.radford.edu>
2018-10-14 14:05:15 -0400

    add game master files

---
+++ 370_Project_Game-master/src/BalloonController.gd
@@ -0,0 +1,270 @@
+#	Code Produced as part of a project for Dr. Joe Chase
+#	Radford University: ITEC370
+#	Date: 	April 22nd, 2018
+#	By: 	Andrew McGuiness, Ryan Kelley, Andrew Albanese, Michael Hall
+#	All rights are reserved by the above entities.
+#
+#	Purpose: This class is responsible for most of the game's core logic.  
+#	The controller will handle a user's input.  The location of the input
+#	will be registered and compared to the target's bounds.  If the click
+#	is within those bounds, the data will be submitted to the database and
+#	a new target will be spawned.  The target that gets spawned is pulled
+#	from a list of targets that is supplied to the program at runtime.
+
+extends Node2D
+
+onready var global = get_node("/root/Global")
+
+# Pixel Dist is the constant used by all screens to scale the targets
+export var PIXEL_DIST = 64
+export var numScenes = 4
+
+# Score handler
+export(NodePath) var scoreLabelPath
+var scoreLabel
+
+# Scenes for the targets
+export(PackedScene) var redBalloon
+export(PackedScene) var blueBalloon
+export(PackedScene) var greenBalloon
+export(PackedScene) var blackBalloon
+
+# Scenes for the text and explosion
+export(PackedScene) var textScene
+export(PackedScene) var explosion
+
+# Data for the current target
+var current
+var windowSize
+var currentRect
+var focusPosition
+
+# Data for the current round
+var targetVisible = false
+var time = 0.0
+var misses = 0
+
+# Track which target is displayed and when one is next
+var balloonNumber = 1
+var balloonID = -1
+var playing = true
+
+# Tracks which targets have already been hit
+var targetsAlreadyHit = []
+
+# Initialize the scene and begin the game
+func _ready():
+	global.getNewID()
+	windowSize = get_viewport().size
+	
+	# Find the central location
+	focusPosition = windowSize / 2
+	balloonID = generateTarget()
+	scoreLabel = get_node(scoreLabelPath)
+	scoreLabel.clearScore()
+
+#Update the target timer
+func _process(delta):
+	if targetVisible:
+		time += delta
+
+# When the user clicks/taps, handle the input
+func _unhandled_input(event):
+	# Handle Click Events
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			mouseClicked(event.position)
+		
+	# Handle Touch events
+	elif event is InputEventScreenTouch:
+		screenTouched(event.position)
+
+# Handle the click/touch event
+func mouseClicked(clickPosition):
+	# Ensure that the game is actually running
+	if !playing:
+		return
+	
+	var distance = clickPosition.distance_to(current.position)
+	
+	# Create the animated text
+	var t = textScene.instance()
+	t.position = current.position
+	add_child(t)
+	
+	# Test to see if the click is inside the target's bounds
+	if currentRect.has_point( clickPosition ):
+		# Display hit over the target
+		t.play("Hit!")
+		
+		var e = explosion.instance()
+		add_child(e)
+		e.position = current.position
+		e.scale = current.get_node("TargetSprite").scale * 2
+		e.play()
+		
+		# Spawn and play a new sound
+		get_parent().get_node("SoundController").playSound()
+		
+		current.queue_free()
+		targetVisible = false
+		
+		scoreLabel.addScore(int(100 * (10/(distance+1))))
+		
+		uploadTargetHit(balloonID, time, misses, balloonNumber)
+		
+		balloonNumber += 1
+		if balloonNumber > global.targetCount:
+			endGame()
+		else:
+			misses = 0
+			time = 0.0
+			balloonID = generateTarget()
+		
+	# The user clicked outside of the bounds, so it was a miss
+	else:
+		# Display miss over the target
+		t.play("Miss!")
+		misses += 1
+
+# Create a new target for the user
+func makeBalloon(spawnPos, sz):
+	# Generate random index
+	var i = randi() % numScenes
+	
+	# Select the new target
+	if i == 0:
+		current = redBalloon.instance()
+	elif i == 1:
+		current = blueBalloon.instance()
+	elif i == 2:
+		current = blackBalloon.instance()
+	else:
+		current = greenBalloon.instance()
+	
+	print("Target Number: ", balloonNumber)
+	
+	current.position = spawnPos
+	add_child(current)
+	
+	# Get a reference to the target
+	var node = current.get_node("TargetSprite")
+	
+	# Scale the target
+	var size = node.texture.get_size()
+	node.scale.x = sz.x / size.x
+	node.scale.y = sz.y / size.y
+	
+	# Move the target into position
+	focusPosition = current.position
+	var pos = current.position
+	pos.x -= sz.x/2
+	pos.y -= sz.y/2
+
+	# Create a rect for the target's bounds checking
+	currentRect = Rect2(pos, sz)
+	targetVisible = true
+
+# Send target data and score to the DB
+func uploadTargetHit( num, time, miss, total):
+	global.uploadTargetHit(num, time, miss, total)
+	global.uploadScore( scoreLabel.currentScore )
+
+# The player has hit the required number of targets, end the round
+func endGame():
+	playing  = false
+	get_parent().get_node("RoundCompletePanel").show()
+	global.uploadScore( scoreLabel.currentScore )
+
+
+# Start a new round for the player
+func _on_PlayAgain_pressed():
+	# Generate a new row in the DB
+	global.getNewID()
+	
+	# hide the modal
+	get_parent().get_node("RoundCompletePanel").hide()
+
+	# reset the round variables
+	balloonNumber = 1
+	targetsAlreadyHit = []
+	focusPosition = windowSize / 2
+	targetVisible = false
+	scoreLabel.clearScore()
+	misses = 0
+	time = 0.0
+
+	# Create a new balloon and display it
+	balloonID = generateTarget()
+	playing  = true
+
+
+# Generate a new valid target
+func generateTarget():
+	var valid = false
+	var targetNum
+	var attempts = 0
+	
+	while !valid and attempts < 100:
+		targetNum = generateUniqueNumber()
+		
+		# Convert the raw target data into real data
+		var direction = directionToVector(global.targets[targetNum].direction)
+		var size = float(global.targets[targetNum].size)
+		var distance = int(global.targets[targetNum].distance)
+		
+		var pos = focusPosition + (direction * (distance-1) * (PIXEL_DIST - 10))
+		var rectSize = size * Vector2(PIXEL_DIST, PIXEL_DIST)
+		
+		var testRect = Rect2(pos, rectSize)
+		
+		valid = get_viewport_rect().encloses(testRect)
+		if valid:
+			#spawn the balloon
+			makeBalloon(pos, rectSize)
+		else:
+			#Sentry to avoid infinite looping
+			attempts += 1
+			if attempts > 10:
+				focusPosition = windowSize / 2
+	
+	if !valid && attempts >= 100:
+		print("Failed to place target")
+		print( targetsAlreadyHit )
+	
+	global.targets[targetNum].printData()
+	targetsAlreadyHit.append(targetNum)
+
+	return targetNum
+
+
+# Convert the plain text from the target into a vector direction
+func directionToVector( dir ):
+	if dir == "NE":
+		return Vector2(1, -1)
+	elif dir == "NW":
+		return Vector2(-1, -1)
+	elif dir == "SE":
+		return Vector2(1, 1)
+	elif dir == "SW":
+		return Vector2(-1, 1)
+
+
+# Generate an ID that hasn't been used yet
+func generateUniqueNumber():
+	var numberValid = false
+	var targetNum = -1
+	
+	while !numberValid:
+		# generate random
+		targetNum = (randi() % global.targetCount + 1) - 1 
+		
+		# check if it's already been used
+		if targetsAlreadyHit.has(targetNum):
+			# already used, regenerate
+			numberValid = false
+		else:
+			# new number, good to use
+			numberValid = true
+	
+	return targetNum
---
+++ 370_Project_Game-master/src/StartScreen.gd
@@ -0,0 +1,84 @@
+#	Code Produced as part of a project for Dr. Joe Chase
+#	Radford University: ITEC370
+#	Date: 	April 22nd, 2018
+#	By: 	Andrew McGuiness, Ryan Kelley, Andrew Albanese, Michael Hall
+#	All rights are reserved by the above entities.
+#
+#	Purpose: The Start Screen is the Main Menu of the game.  It is the first
+#	thing that is displayed to the user.  This script is designed to handle the
+#	click events for all of the buttons on the Main Menu.  This script also 
+#	initializes the theme assets and loads them into memory.  The start script
+#	will also download and begin parsing the target file.
+
+extends Control
+
+onready var global = get_node("/root/Global")
+
+#Themes to use
+export(Resource) var carnivalTheme
+export(Resource) var spaceTheme
+
+#Background to use
+export(Texture) var spaceBackground
+export(Texture) var carnivalBackground
+
+#When the program loads, prep the theme and targets
+func _ready():
+	#Download and parse the target JSON file
+	if !global.targetsParsed:
+		$HTTPRequest.request(global.address + "target_hunter/targetFile.json")
+	
+	#Set the theme and background
+	$Panel.theme = global.currentTheme
+	$background.texture = global.currentBackground
+
+#Start a new game for the user
+func _on_New_Game_pressed():
+	#Ensure the target parser is done, then change the scene
+	if global.targetsParsed:
+		get_tree().change_scene("res://screens/PlayScreen.tscn")
+
+	#Target parser is not done, need to wait
+	else:
+		print("Loading Targets")
+
+#Change to the high score scene
+func _on_High_Scores_pressed():
+	get_tree().change_scene("res://screens/ScoreScreen.tscn")
+
+#When the target file is done downloading, parse it
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	global.parseTargets( body.get_string_from_utf8() )
+
+
+#Change the theme based on which is currently is
+func _on_ChangeTheme_pressed():
+	if global.availableThemes == "Both":
+		#Switch to Space
+		if global.themeLabel == "Carnival":
+			global.themeLabel   = "Space"
+			global.currentTheme = spaceTheme
+			global.currentBackground = spaceBackground
+		
+		#Switch to Carnival
+		elif global.themeLabel == "Space":
+			global.themeLabel   = "Carnival"
+			global.currentTheme = carnivalTheme
+			global.currentBackground = carnivalBackground
+		
+		#Update the background and the UI to match the current theme
+		$background.texture = global.currentBackground
+		$Panel.theme = global.currentTheme
+	else:
+		print("Theme restricted by admin")
+
+
+#Exit button was clicked, try to exit the game
+func _on_Exit_pressed():
+	#If we are in an HTML file, then call the javascript
+	if global.jsEnv:
+		JavaScript.eval("exit()", true)
+	
+	#Otherwise, just close the window
+	else:
+		get_tree().quit()
